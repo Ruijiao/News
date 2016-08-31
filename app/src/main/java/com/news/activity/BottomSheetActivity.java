177dbf8b51095f1256091bd2ce1555d6044ce449
ruijiao <1160676773@qq.com>
2016-08-31 18:27:52 +0800

    添加测试bottom sheet 和 network库

---
+++ app/src/main/java/com/news/activity/BottomSheetActivity.java
@@ -0,0 +1,19 @@
+package com.news.activity;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+import com.news.R;
+
+/**
+ * Created by xiangha on 2016/8/29.
+ */
+
+public class BottomSheetActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.a_bottom_sheet);
+    }
+}
